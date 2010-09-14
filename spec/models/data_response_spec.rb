057a7899878176da6a692ce4da821d8ba48b356a
Glenn Roberts <glenn.roberts@siyelo.com>
2010-09-15 01:57:24 +1000

    get tests green

--- spec/models/data_response_spec.rb
+++ spec/models/data_response_spec.rb
@@ -9,21 +9,21 @@ describe DataResponse do
   
   describe "date validations" do
     it "accepts start date < end date" do
-      dr = Factory(:data_response, 
+      dr = Factory.build(:data_response, 
                     :fiscal_year_start_date => DateTime.new(2010, 01, 01),
                     :fiscal_year_end_date => DateTime.new(2010, 01, 02) )
       dr.should be_valid
     end
 
     it "does not accept start date > end date" do
-      dr = Factory(:data_response, 
+      dr = Factory.build(:data_response, 
                     :fiscal_year_start_date => DateTime.new(2010, 01, 02),
                     :fiscal_year_end_date => DateTime.new(2010, 01, 01) )
       dr.should_not be_valid
     end
 
     it "does not accept start date = end date" do
-      dr = Factory(:data_response, 
+      dr = Factory.build(:data_response, 
                     :fiscal_year_start_date => DateTime.new(2010, 01, 01),
                     :fiscal_year_end_date => DateTime.new(2010, 01, 01) )
       dr.should_not be_valid
