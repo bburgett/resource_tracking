af79e6fd0476e2cd0d63825694f310954d337042
Glenn Roberts <glenn.roberts@siyelo.com>
2010-09-15 01:30:58 +1000

    add failing tests for sd/ed

--- spec/factories/data_response.rb
+++ spec/factories/data_response.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__),'./blueprint.rb')
 
 Factory.define :data_response, :class => DataResponse do |f|
-  f.organization  { Factory.create(:organization) }
-  f.currency      { 'USD' }
+  f.responding_organization  { Factory.create(:organization) }
+  f.currency                 { 'USD' }
 end
--- spec/models/data_response_spec.rb
+++ spec/models/data_response_spec.rb
@@ -1,9 +1,33 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe DataResponse do
-
-    describe "validations" do
-      it { should validate_presence_of(:fiscal_year_start_date) }
-      it { should validate_presence_of(:fiscal_year_end_date) }
+  
+  describe "basic validations" do
+    it { should validate_presence_of(:fiscal_year_start_date) }
+    it { should validate_presence_of(:fiscal_year_end_date) }
+  end
+  
+  describe "date validations" do
+    it "accepts start date < end date" do
+      dr = Factory(:data_response, 
+                    :fiscal_year_start_date => DateTime.new(2010, 01, 01),
+                    :fiscal_year_end_date => DateTime.new(2010, 01, 02) )
+      dr.should be_valid
     end
+
+    it "does not accept start date > end date" do
+      dr = Factory(:data_response, 
+                    :fiscal_year_start_date => DateTime.new(2010, 01, 02),
+                    :fiscal_year_end_date => DateTime.new(2010, 01, 01) )
+      dr.should_not be_valid
+    end
+
+    it "does not accept start date = end date" do
+      dr = Factory(:data_response, 
+                    :fiscal_year_start_date => DateTime.new(2010, 01, 01),
+                    :fiscal_year_end_date => DateTime.new(2010, 01, 01) )
+      dr.should_not be_valid
+    end
+
+  end
 end
